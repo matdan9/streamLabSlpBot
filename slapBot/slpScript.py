78a1865be95f3c1988a57e9a53d335baaa6877e9
matdan <matdanchicototo@gmail.com>
2021-01-03 19:25:52 +0000

    integrated configs + pulse feedback on configured alerts

--- slapBot/slpScript.py
+++ slapBot/slpScript.py
@@ -3,11 +3,17 @@ import RPi.GPIO as GPIO
 import time
 import socketio
 import sys
-import asyncio	
+import json
 
 sio = socketio.Client()
 motorPin = 7
-GPIO.cleanup()
+configuration = []
+
+class Config:
+	name = None
+	value = 1
+	on = False
+
 
 @sio.event
 def connect():
@@ -16,8 +22,16 @@ def connect():
 
 @sio.on('event')
 def event(data):
-    print("received data from streamlabs:")
-    print(data)
+	global configuration
+	for cs in configuration:
+		if(cs.name == data['type'] and cs.on):
+			try:
+				if(cs.value <= data['message'][0]['amount']):
+					slap(len(data['message']))
+			except:
+				slap(len(data['message']))
+	print("received data from streamlabs:")
+	print(data)
 
 
 @sio.event
@@ -29,34 +43,52 @@ def disconnect():
 def main():
 	print("starting slapping bot!")
 	print("GET READY")
+	loadConfig()
 	if(len(sys.argv) < 2 or sys.argv[1] is None):
 		print("you must init this with an socketapi token")
 		return
 	initSocketio()
 	initGpio()
-	GPIO.output(motorPin, GPIO.HIGH)
-	time.sleep(10)
-	GPIO.output(motorPin, GPIO.LOW)
 
+
+def slap(force = 1):
+	GPIO.output(motorPin, GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(motorPin, GPIO.LOW)
 
 
 def initSocketio():
 	print("setting up socket.io")
 	global sio
 	sio.connect("https://sockets.streamlabs.com?token=" + sys.argv[1], transports="websocket");
-	#sio.wait()
 
 def initGpio():
 	print("setting up motor")
 	GPIO.setmode(GPIO.BOARD)
 
 	GPIO.setup(motorPin, GPIO.OUT)
-	#GPIO.output(18, GPIO.HIGH)
-	#GPIO.output(18, GPIO.LOW)
 
+def loadConfig():
+    fileContent = getConfigFileContent()
+    global configuration	
+    c = json.loads(fileContent)
+    for conf in c:
+        nConf = Config()
+        nConf.name = conf['name']
+        nConf.value = conf['value']
+        nConf.on = conf['on']
+        configuration.append(nConf)
+
+def getConfigFileContent():
+	f = None;
+	try:
+		f = open("/opt/slpConfig.json")
+	except:
+		f = open("./slapBot/slpConfig.json")
+	return f.read()
 
 try:
-    print("set GIOP high")
+    print("preparing bot")
 
 
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
